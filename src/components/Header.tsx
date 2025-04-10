bdd12d5e9b2ce2e2b5cc1805fb7a204c27b13bf5
santhosh <santhosh.003z@gmail.com>
2025-04-10 18:40:55 +0530

    feat: added Box component in utils

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,36 @@
+import Box from "../../utils/Box";
+
+function Timer({ time }) {
+  return <div className="w-[200px] text-xl">Time: {time}</div>;
+}
+
+function Icons({ chars }) {
+  return (
+    <ul className="p-2">
+      {chars.map((char: {}) => {
+        return (
+          <li key={char.name}>
+            <img
+              src={"/assets/" + char.name + ".png"}
+              alt={char.name}
+              className={
+                "w-[50px] h-[50px]" + (char.found ? " opacity-[.5]" : "")
+              }
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 function Header({ chars, time }) {
   return (
-    <header className="header text-2xl font-semibold p-3 fixed rounded-lg flex justify-evenly items-center w-screen bg-black text-white">
-      <div>
-        <ul>
-          {chars.map((char: {}) => {
-            return (
-              <li key={char.name}>
-                <img
-                  src={"/assets/" + char.name + ".png"}
-                  alt={char.name}
-                  className={
-                    "w-[50px] h-[50px]" + (char.found ? " opacity-[.5]" : "")
-                  }
-                />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="w-[200px] text-xl">Time: {time}</div>
+    <header className="header text-2xl font-semibold p-3 fixed rounded-lg flex justify-evenly items-center w-screen">
+      <div></div>
+      {/* <div className="w-[200px] text-xl">Time: {time}</div> */}
+      <Box children={<Icons chars={chars} />} />
+      <Box children={<Timer time={time} />} />
     </header>
   );
 }
---
+++ utils/Box.jsx
@@ -0,0 +1,9 @@
+function Box({ children }) {
+  return (
+    <div className="flex justify-center items-center p-2 border-2 rounded-lg border-amber-400 bg-black/75 text-white shadow-[1px_1px_19px_0px] shadow-amber-400">
+      {children}
+    </div>
+  );
+}
+
+export default Box;
