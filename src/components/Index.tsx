f7e1669e510d3f2afd12e0fe4cea4995bd00f2a5
santhosh <santhosh.003z@gmail.com>
2025-04-11 15:59:48 +0530

    feat: added styling to home page

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,19 +1,29 @@
 import { Link } from "react-router";
+import Box from "../../utils/Box";
 
 function Index() {
-  return (
-    <main className="flex justify-center items-center h-screen">
-      <section className="flex flex-col justify-center items-center gap-5">
+  function Home() {
+    return (
+      <div className="flex flex-col justify-center items-center gap-5 p-5">
         <h1 className="font-semibold text-3xl">Find Where's Waldo</h1>
         <div className="text-2xl flex items-center gap-2">
-          <button className="bg-gray-100 py-2 px-3 rounded-lg">
+          <button className="bg-gray-600 py-2 px-3 rounded-lg hover:bg-gray-700">
             <Link to={`/game`}>Start Game</Link>
           </button>
-          <button className="bg-gray-100 py-2 px-3 rounded-lg">
+          <button className="bg-gray-600 py-2 px-3 rounded-lg hover:bg-gray-700">
             <Link to={`/leaderboard`}>LeaderBoard</Link>
           </button>
         </div>
-      </section>
+      </div>
+    );
+  }
+
+  return (
+    <main className="">
+      <div className="bg-[url(/assets/main.jpg)] blur-xs h-screen"></div>
+      <div className="fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
+        <Box children={<Home />} />
+      </div>
     </main>
   );
 }
